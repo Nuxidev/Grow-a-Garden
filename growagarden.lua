ac44a098f0ac687671cd7154364ee7abe8690327
Nuxidev <198355688+Nuxidev@users.noreply.github.com>
2025-07-22 19:43:55 +0200

    Update growagarden.lua

--- growagarden.lua
+++ growagarden.lua
@@ -1,4 +1,4 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/main/main.lua"))()
 
 local Window = Fluent:CreateWindow({
     Title = "Nuxi Hub | Grow a Garden 🌱",
